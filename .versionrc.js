338933cfd25774a6f7ddd22bf3a5d80d3f49a27e
zixiCat <zixicat@qq.com>
2020-11-24 15:44:41 +0800

    feat: link start

---
+++ .versionrc.js
@@ -0,0 +1,27 @@
+/**
+ * devDependencies: standard-version
+ * */
+const settings = require(process.cwd() + "/package.json");
+
+module.exports = {
+  header: `# Changelog
+
+All notable changes to this project will be documented in this file.
+`,
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", hidden: true },
+    { type: "docs", hidden: true },
+    { type: "style", hidden: true },
+    { type: "refactor", hidden: true },
+    { type: "perf", hidden: true },
+    { type: "test", hidden: true },
+  ],
+  commitUrlFormat: settings.homepage + "/commit/{{hash}}",
+  compareUrlFormat:
+    settings.homepage + "/compare/{{previousTag}}...{{currentTag}}",
+  issueUrlFormat: settings.homepage + "/issues/{{id}}",
+  userUrlFormat: settings.homepage + "",
+  releaseCommitMessageFormat: "v{{currentTag}}",
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+# config-pack
+
+My config pack, which contains many configurations for personal use. Hope it can help you.
+
+Assuming you'd like one of them, you could use following **PowerShell** command line to download it to current path.
+
+## Intro
+
+#### [.eslintrc.js](https://github.com/eslint/eslint)
+
+Find and fix problems in your JavaScript code
+
+```bash
+powershell -command "Invoke-WebRequest 'https://raw.githubusercontent.com/zixiCat/config-pack/master/.eslintrc.js' -OutFile '.eslintrc.js'"
+```
+
+#### [.versionrc.js](https://github.com/conventional-changelog/standard-version)
+
+A utility for versioning using semver and CHANGELOG generation powered by Conventional Commits.
+
+```bash
+powershell -command "Invoke-WebRequest 'https://raw.githubusercontent.com/zixiCat/config-pack/master/.versionrc.js' -OutFile '.versionrc.js'"
+```
