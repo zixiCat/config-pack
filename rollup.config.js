4bc6a037dff7b2f1106e38eba2f081e5c2cf9951
zixiCat <zixicat@qq.com>
2021-07-20 20:17:02 +0800

    chore: add shebangPlugin

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,21 @@ export default {
       sourcemap: process.env.NODE_ENV !== 'prod',
     },
   ],
-  plugins: [typescript(), terser()],
+  plugins: [shebangPlugin(), typescript(), terser()],
   external: ['fs', 'path', 'child_process'],
 };
+
+/**
+ * add shebang line to top of index
+ * @return {string} final code
+ */
+function shebangPlugin() {
+  return {
+    name: 'shebangPlugin',
+    renderChunk(code, chunk) {
+      const id = chunk.fileName;
+      if (id !== 'index.js') return null;
+      return '#!/usr/bin/env node\n' + code;
+    },
+  };
+}
--- scripts/build.js
+++ scripts/build.js
@@ -9,16 +9,6 @@ const sh = execSync;
 
   // prepare source
   sh('npx rollup -c');
-  addShebangLineSync();
 })().catch((err) => {
   if (err) throw err;
 });
-
-/**
- * add shebang line to top of index.js
- */
-function addShebangLineSync() {
-  const data = fs.readFileSync('./lib/index.js', 'utf-8');
-  const newValue = '#!/usr/bin/env node\n' + data;
-  fs.writeFileSync('./lib/index.js', newValue, 'utf-8');
-}
